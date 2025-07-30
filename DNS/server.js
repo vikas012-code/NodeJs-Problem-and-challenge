bd0c066e9fbff68b97afa919e107b09e205c4f5a
vikas thapa <vikasthapa115@email.com>
2025-07-30 22:55:37 +0530

    dns lookup developed

---
+++ DNS/server.js
@@ -0,0 +1,22 @@
+const http = require("http");
+const dns= require("dns");
+
+const port="8000"
+const host="localhost"
+
+const server = http.createServer((req, res) => {
+    const param=req.url.split("=")
+
+    console.log("Server is running...")
+    if(req.url==="/"){
+        res.end("this is home page \nto search go to /lookup?domain='searching domain name' ")
+    }
+    if(param.length>1){
+        dns.lookup(param[1],4, (err, address, family) => {
+        console.log('address: %j family: IPv%s', address, family);
+        res.end(` IPV${family} Address of ${param[1]} is ${address} \n\n or \n\n {\n domain:${param[1]},\n IP:${address}\n }`)
+        });
+    }
+});
+
+server.listen(port,host);
