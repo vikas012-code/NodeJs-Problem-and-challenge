5d6d8013af31982a3c7bbe257629b934baaba616
vikas thapa <vikasthapa115@email.com>
2025-07-31 23:25:17 +0530

    changed message in dns file

--- DNS/server.js
+++ DNS/server.js
@@ -13,7 +13,7 @@ const server = http.createServer((req, res) => {
     }
     if(param.length>1){
         dns.lookup(param[1],4, (err, address, family) => {
-        console.log('address: %j family: IPv%s', address, family);
+        
         res.end(` IPV${family} Address of ${param[1]} is ${address} \n\n or \n\n {\n domain:${param[1]},\n IP:${address}\n }`)
         });
     }
